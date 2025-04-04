2297e90395f30da2a378da5e60932257d4499617
Keegan Smith <keeganasmith2003@gmail.com>
2025-04-04 11:33:33 -0500

    use HPRC resources

--- packages/jupyter-ai-test/jupyter_ai_test/test_llms.py
+++ packages/jupyter-ai-test/jupyter_ai_test/test_llms.py
@@ -4,7 +4,7 @@ from typing import Any, Iterator, List, Optional
 from langchain_core.callbacks.manager import CallbackManagerForLLMRun
 from langchain_core.language_models.llms import LLM
 from langchain_core.outputs.generation import GenerationChunk
-
+import requests
 
 class TestLLM(LLM):
     model_id: str = "test"
@@ -20,8 +20,17 @@ class TestLLM(LLM):
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any,
     ) -> str:
-        time.sleep(3)
-        return f"Hello! This is a dummy response from a test LLM."
+        def send_question(prompt):
+            url = "http://10.72.10.12:5000/infer"
+            headers = {"Content-Type": "application/json"}
+            data = {
+                "input": prompt,
+                "length": 512
+            }
+            response = requests.post(url, headers=headers, json=data)
+            return response.json()["response"]
+        return send_question(prompt)
+
 
 
 class TestLLMWithStreaming(LLM):

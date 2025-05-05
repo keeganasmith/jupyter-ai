d78e2a8105f1b10d66f676aa1425e8869dfe914f
Keegan Smith <keeganasmith@gmail.com>
2025-05-05 12:33:11 -0500

    for aces

--- packages/HPRC-llama-8B/HPRC_llama_8B/llm.py
+++ packages/HPRC-llama-8B/HPRC_llama_8B/llm.py
@@ -2,8 +2,10 @@ from typing import Any, List, Optional
 
 from langchain_core.callbacks.manager import CallbackManagerForLLMRun
 from langchain_core.language_models.llms import LLM
-
+import os
 import requests
+import subprocess
+import pickle
 
 class Llama_8B_LLM(LLM):
     model_id: str = "HPRC_llama_8B"
@@ -21,6 +23,11 @@ class Llama_8B_LLM(LLM):
     ) -> str:
         def send_question(prompt):
             url = "http://10.72.10.19:5000/infer"
+            cluster=str(subprocess.run(['/sw/local/bin/clustername'], stdout=subprocess.PIPE).stdout.decode('utf-8').lower()).strip()
+            if(cluster == "aces"):
+                with open("/sw/hprc/sw/dor-hprc-venv-manager/codeai/ip.pkl", "rb") as my_file:
+                    ip = pickle.load(my_file)
+                    url = f"http://{ip}:5000/infer"
             headers = {"Content-Type": "application/json"}
             data = {
                 "input": prompt,
